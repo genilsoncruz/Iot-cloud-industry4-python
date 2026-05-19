a943de8d25e2b710be0034fa73024948a400952b
Genilson de Morais Cruz <genilsoncruz@gmail.com>
2026-05-19 11:22:56 -0300

    Update co2-sensor-exemplo1.cpp

--- aula10/co2/co2-sensor-exemplo1.cpp
+++ aula10/co2/co2-sensor-exemplo1.cpp
@@ -1,28 +1,40 @@
-const int pwmPin = 9;  // Pino conectado ao pino PWM do sensor SC8
-int ppm = 0;           // Variável para armazenar a concentração de CO2
+#include <Arduino.h>
+
+const int pwmPin = D5;
+
+int ppm = 0;
 
 void setup() {
+
   Serial.begin(9600);
+
   pinMode(pwmPin, INPUT);
+
   Serial.println("Iniciando leitura do sensor SC8...");
 }
 
 void loop() {
-  // Lê a duração do pulso em nível alto (HIGH) em microssegundos
-  // O período total do PWM do SC8 é de 1004 ms (1004000 microssegundos)
-  unsigned long highTime = pulseIn(pwmPin, HIGH, 1050000);
-  
-  // Fórmula: PPM = 10000 * (Thigh - 2ms) / 1004ms
-  // Subtraímos 2000 microssegundos (2ms) do tempo alto e dividimos pelo período total
+
+  // Timeout ~1.1s
+  unsigned long highTime =
+    pulseIn(pwmPin, HIGH, 1100000);
+
   if (highTime > 0) {
-    ppm = 10000 * (highTime - 2000) / 1004000;
-    
-    Serial.print("Concentracao de CO2: ");
+
+    ppm =
+      10000 * (highTime - 2000)
+      / 1004000;
+
+    Serial.print("CO2: ");
     Serial.print(ppm);
-    Serial.println(" PPM");
+    Serial.println(" ppm");
+
   } else {
-    Serial.println("Erro: Sensor nao conectado ou sem pulso detectado.");
+
+    Serial.println(
+      "Erro: sem pulso PWM"
+    );
   }
 
-  delay(2000); // Aguarda 2 segundos entre as leituras
+  delay(2000);
 }
